b9ce5f6c9969ac7bd29fb48d4dfb16bc105ddc19
David Maldonado <david.maldonado@momseguros.co>
2022-11-12 11:27:28 -0500

    Creations of all users login register in Users

--- API/APPS/users/api/serializers.py
+++ API/APPS/users/api/serializers.py
@@ -1,16 +1,27 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework import serializers, validators
-
+from APPS.users.models import User
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ('username', 'password', 'email', 'first_name', 'last_name')
 
         extra_kwargs = {
             'password': {'write_only': True},
             'email': {'validators': [validators.UniqueValidator(
-                User.objects.all(), "email already exists"
+                get_user_model().objects.all(), "email already exists"
                 )]}}
         def create(self, validated_data):
-            user = User.objects.create_user(**validated_data)
+            user = get_user_model().objects.create_user(**validated_data)
             return user
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        return {
+            'username': instance.username,
+            'email': instance.email,
+            'password': instance.password,
+        }
--- API/APPS/users/api/urls.py
+++ API/APPS/users/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from API.APPS.users import views
+from .api import login_api, register_api, get_user_data
 from knox import views as knox_views
 urlpatterns = [
-    path('login/', views.login_api, name='login'),
-    path('user/', views.get_user_data, name='get_user_data'),
-    path('register/', views.register_api, name='register'),
+    path('login/', login_api, name='login'),
+    path('user/', get_user_data, name='get_user_data'),
+    path('register/', register_api, name='register'),
     path('logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
 ]
